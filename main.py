368e82fc98510c38ae1b5d05c347ce6edde01989
saurav-tiwari03 <sauravtiwari5625@gmail.com>
2025-04-26 02:54:42 +0530

    Add new endpoints for item retrieval and food categorization

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Request
+from enum import Enum
+
 
 app = FastAPI()
 
@@ -11,3 +13,24 @@ def count(request: Request):
     data = request.json()
     print(data)
     return {"message": "Hello World2"}
+
+@app.get("/items/{item_id}")
+async def items(item_id:str):
+    return {"message": f"Item ID: {item_id}"}
+
+class FoodItems(str,Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+@app.get("/food/{food_item}")
+async def food_item(food_item: FoodItems):
+    if food_item == FoodItems.fruits:
+        return ["Apple","Banana","Orange"]
+    elif food_item == FoodItems.vegetables:
+        return ["Carrot","Potato","Onion"]
+    else:
+        return ["Milk","Cheese","Yogurt"]
+    
+    
+    
